284b60c83149af667ce1f08d28376bac221473b5
Niranjan <niranjancb2035@gmail.com>
2025-10-13 00:03:21 +0530

    updated frontend environment variables

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const BACKEND_BASE = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
-export const socket = io(BACKEND_BASE, {
+export const socket = io('https://ai-chat-app-wb7u.onrender.com', {
   // autoConnect: false,
   transports: ["websocket"],
 });
